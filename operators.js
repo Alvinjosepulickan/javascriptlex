3c69ab824b398958c91fbee7b1ef0b63b8d428ef
Alvin Jose <alvjose@publicisgroupe.net>
2023-01-26 13:32:25 +0530

    statements and expression

---
+++ operators.js
@@ -0,0 +1,14 @@
+let sum = 5 + 10; 
+//Arithmentic   + - * / % ++ --
+//Assignment = += -= *= /= %=
+//logical && || !
+//relational > >= < <= == != ===
+//conditional
+
+
+
+let noOfSeatsBooked=3;
+let costOfThreeTickets=27;
+let festiveSeaonDiscount=0.1;
+let totalPrice=costOfThreeTickets-(costOfThreeTickets*festiveSeaonDiscount);
+console.log(`Total Price is ${totalPrice}$`);
---
+++ statements and expressions.js
@@ -0,0 +1,39 @@
+// statements-> conditional and non-conditional
+//non-conditional- continue, break,comment
+// comment // */
+//conditional if-else && switch case
+
+//switch
+/*
+switch(condition){
+
+    case 1:
+        break;
+    case 2:
+        break;
+    default:
+        break;
+}
+*/
+/*
+var perfRating = 5;
+switch (perfRating) {
+    case 5:
+        console.log("Very Poor");
+        break;
+    case 4:
+        console.log("Needs Improvement");
+        break;
+    case 3:
+        console.log("Met Expectations");
+        break;
+    case 2:
+        console.log("Commendable");
+        break;
+    case 1:
+        console.log("Outstanding");
+        break;
+    default:
+        console.log("Sorry!! Invalid Rating.");
+}
+*/
