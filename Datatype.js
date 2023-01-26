e7f38c3cc74aea6d7299a49f3cd6c2d448680231
Alvin Jose <alvjose@publicisgroupe.net>
2023-01-26 12:43:48 +0530

    datatypes

---
+++ Datatype.js
@@ -0,0 +1,23 @@
+let age = 24; //number  
+let name = "Tom" //string  
+let qualified = true; //boolean 
+//number ,string, boolean,null, unidentified ,object, symbols
+
+//string literals
+let firstName="Kevin";
+let lastName="Patrick";
+console.log(`Name:${firstName} ${lastName}
+Email:${firstName}_${lastName}@abc.com`);
+/*
+OUTPUT:
+Name: Kevin Patrick
+Email:Kevin_Patrick@abc.com 
+*/
+
+
+let movie={
+    Name:"Twilight",
+    Language:"English",
+    Rating:4.5
+}
+console.log(`Movie Name is ${movie.Name} \n Language is ${movie.Language} \n Rating is ${movie.Rating}`)
